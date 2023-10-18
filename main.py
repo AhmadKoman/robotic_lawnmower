be61188294feaebc5a6dcfba0e8195a020e36c37
HadiSaleh02 <148077185+HadiSaleh02@users.noreply.github.com>
2023-10-18 10:20:12 +0000

    hi

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ import os
 def plot_map(rows, cols):
     with open (simple.csv, "r", encoding="utf-8") as file:
         plot_map.reverse()
-
         rows = len(plot_map)
         cols = len(plot_map[0])
