245aebb96001576a05956dfd92faf661697384d5
LORDPRETTYFLVCKOJODYE11 <121387659+LORDPRETTYFLVCKOJODYE11@users.noreply.github.com>
2023-10-18 10:11:42 +0000

    idk

---
+++ main.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import os
+def plot_map(rows, cols):
+    with open (simple.csv, "r", encoding="utf-8") as file:
+        plot_map.reverse()
+
+        rows = len(plot_map)
+        cols = len(plot_map[0])
