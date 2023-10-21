c1540f010b74587db84d96390a286b96c640f65e
HadiSaleh02 <148077185+HadiSaleh02@users.noreply.github.com>
2023-10-21 19:26:35 +0000

    hej

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
-import os
 import csv
-import numpy as np
-def plot_map(rows, cols):
-    path = "/simple.csv"
-    with open (path , "r", encoding="utf-8") as file:
-        plot_map.reverse()
-        rows = len(plot_map)
-        cols = len(plot_map[0])
-    
-    rows, cols = plot_map.shape
-    N = 10
-    cov = np.zeros((rows * N, cols * N))
-    
+import os
+
+def read_ground_map(filename):
+    ground_map = []
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            ground_map.append(row)
+    return ground_map
+
+current_directory = os.getcwd()
+
+filename = os.path.join(current_directory, "simple.csv")
+
+map = read_ground_map(filename)
+print(map)
+
+color_map = {'L': 'blue',  'O': 'orange', 'S': 'green'}
+
+
+import matplotlib.pyplot as plt
+
+
+def plot_colored_map(ground_map, color_map):
+    rows = len(ground_map)
+    cols = len(ground_map[0])
+    colored_map = []
+
+    for row in ground_map:
+        colored_row = [color_map[cell] for cell in row]
+        colored_map.append(colored_row)
+
+    plt.imshow(colored_map, interpolation='nearest')
+    plt.show()
+
+# Plot the colored map
+plot_colored_map(map, color_map)
--- test_hadi.py
+++ test_hadi.py
@@ -6,7 +6,9 @@ def read_ground_map(filename):
     with open(filename, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            ground_map.append(row)
+            # Strip leading and trailing whitespace from each cell
+            cleaned_row = [cell.strip() for cell in row]
+            ground_map.append(cleaned_row)
     return ground_map
 
 # Get the current working directory
@@ -16,4 +18,30 @@ current_directory = os.getcwd()
 filename = os.path.join(current_directory, "simple.csv")
 
 hej = read_ground_map(filename)
-print(hej)
+
+# Define a color map
+color_map = {
+    'L': 'blue',
+    'O': 'orange',
+    'S': 'green'
+}
+
+import matplotlib.pyplot as plt
+
+# Define a function to plot the colored grid
+def plot_colored_map(ground_map, color_map):
+    rows = len(ground_map)
+    cols = len(ground_map[0])
+    colored_map = []
+
+    for row in ground_map:
+        colored_row = [color_map[cell] for cell in row]
+        colored_map.append(colored_row)
+
+    plt.imshow(colored_map, interpolation='nearest')
+    plt.show()
+
+# Plot the colored map
+plot_colored_map(hej, color_map)
+
+
