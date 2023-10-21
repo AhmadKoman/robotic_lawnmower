cc3176eab5d4eb8355c69f22aa6a5eac476b5a93
HadiSaleh02 <148077185+HadiSaleh02@users.noreply.github.com>
2023-10-21 20:07:59 +0000

    hej

--- main.py
+++ main.py
@@ -1,38 +1,57 @@
-import csv
 import os
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
-def read_ground_map(filename):
-    ground_map = []
-    with open(filename, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            ground_map.append(row)
-    return ground_map
+
+def read_csv_map(path):
+    plot_map_list = []
+    with open(path, "r") as file:
+        for line in file:
+            row = []
+            for a in line.strip().split(","):
+                if a == "L":
+                    row.append(1)
+                elif a == "O":
+                    row.append(0)
+                else:
+                    row.append(2)
+            plot_map_list.append(row)
+    plot_map_list.reverse()
+    return plot_map_list
+
+
+def is_outside(x, y):
+    global map
+    if 0 <= x <= len(map[0]) and 0 <= y <= len(map):
+        if map[int(y)][int(x)] == 0:
+            return True
+        return False
+    return True
+
+
+
+def visualize_map(map):
+    rows = len(map)  # amount of rows in plot list
+    cols = len(map[0])  # amount of coloms in plot list
+    plt.figure()
+    # Assign color to value: 0 = black, 1 = green, 2 = yellow, 3 = red
+    col_map = ListedColormap(['black', 'green', 'yellow'], 'indexed')
+    # Plot grid
+    plt.pcolormesh(map, edgecolors='k', linewidth=2, cmap=col_map)
+    # Fine tune plot layout
+    ax = plt.gca()  # Get current axis object
+    ax.set_yticks(range(0, rows+1, 1))
+    ax.set_xticks(range(0, cols+1, 1))
+    plt.title(f"Colored grid of size {rows}x{cols}")
+    plt.show()
+    return None
+
 
 current_directory = os.getcwd()
 
-filename = os.path.join(current_directory, "simple.csv")
+path = os.path.join(current_directory, "simple.csv")
 
-map = read_ground_map(filename)
+map = read_csv_map(path)
 print(map)
 
-color_map = {'L': 'blue',  'O': 'orange', 'S': 'green'}
-
-
-import matplotlib.pyplot as plt
-
-
-def plot_colored_map(ground_map, color_map):
-    rows = len(ground_map)
-    cols = len(ground_map[0])
-    colored_map = []
-
-    for row in ground_map:
-        colored_row = [color_map[cell] for cell in row]
-        colored_map.append(colored_row)
-
-    plt.imshow(colored_map, interpolation='nearest')
-    plt.show()
-
-# Plot the colored map
-plot_colored_map(map, color_map)
+visualize_map(map)
--- test_hadi.py
+++ test_hadi.py
@@ -1,34 +1,5 @@
-import csv
-import os
 
-def read_ground_map(filename):
-    ground_map = []
-    with open(filename, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            # Strip leading and trailing whitespace from each cell
-            cleaned_row = [cell.strip() for cell in row]
-            ground_map.append(cleaned_row)
-    return ground_map
 
-# Get the current working directory
-current_directory = os.getcwd()
-
-# Provide the relative path to the CSV file
-filename = os.path.join(current_directory, "simple.csv")
-
-hej = read_ground_map(filename)
-
-# Define a color map
-color_map = {
-    'L': 'blue',
-    'O': 'orange',
-    'S': 'green'
-}
-
-import matplotlib.pyplot as plt
-
-# Define a function to plot the colored grid
 def plot_colored_map(ground_map, color_map):
     rows = len(ground_map)
     cols = len(ground_map[0])
@@ -42,6 +13,5 @@ def plot_colored_map(ground_map, color_map):
     plt.show()
 
 # Plot the colored map
-plot_colored_map(hej, color_map)
-
+plot_colored_map(map, color_map)
 
