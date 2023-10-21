7c5ac0571017936571e591a092bd12a0e4252e0e
LORDPRETTYFLVCKOJODYE11 <121387659+LORDPRETTYFLVCKOJODYE11@users.noreply.github.com>
2023-10-21 17:31:22 +0000

    ww

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 import csv
+import numpy as np
 def plot_map(rows, cols):
     path = "/simple.csv"
     with open (path , "r", encoding="utf-8") as file:
         plot_map.reverse()
         rows = len(plot_map)
         cols = len(plot_map[0])
+    
+    rows, cols = plot_map.shape
+    N = 10
+    cov = np.zeros((rows * N, cols * N))
+    
